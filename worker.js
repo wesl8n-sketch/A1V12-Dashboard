545db6a08bf7a35c6a740635fa7cf087b03ee50e
wesl8n-sketch <wesl8n@gmail.com>
2026-07-04 09:59:20 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,10 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === "/" || url.pathname === "") {
+      url.pathname = "/A1V12_Yahoo_Production_v3_2_Dashboard.html";
+      return env.ASSETS.fetch(new Request(url, request));
+    }
     return env.ASSETS.fetch(request);
   }
 };
